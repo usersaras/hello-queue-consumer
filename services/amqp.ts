69af6d2839b80586f98060ba6caf8511d866560d
usersaras <saras.sthapitshrestha@gmail.com>
2023-11-30 11:32:27 +0545

    feat: Configure job to run again when error is thrown

--- services/amqp.ts
+++ services/amqp.ts
@@ -21,9 +21,53 @@ let chan: amqplib.Channel;
 
     chan.consume(QUEUE, (msg) => {
       if (msg) {
-        console.log(`Received ${msg.content.toString()}`);
-        //perform intensive operations
-        chan.ack(msg);
+        try {
+          if (
+            msg.properties.headers["x-retry-count"] == 5 &&
+            msg.content.toString().includes("1" || 1)
+          ) {
+            console.log("Solved issue");
+            console.log(`Received ${msg.content.toString()}`);
+
+            chan.ack(msg);
+          } else {
+            if (msg.content.toString().includes("1" || 1)) {
+              throw new Error("Sim error!");
+            } else {
+              console.log(`Received ${msg.content.toString()}`);
+              chan.ack(msg);
+            }
+          }
+        } catch (error) {
+          console.error(`Error processing job: ${error}`);
+
+          console.log(!msg.properties.headers["x-retry-count"]);
+
+          if (
+            !msg.properties.headers["x-retry-count"] ||
+            msg.properties.headers["x-retry-count"] < 8
+          ) {
+            const retryCount =
+              (msg.properties.headers["x-retry-count"] || 0) + 1;
+
+            console.log("Retrying ", retryCount);
+
+            setTimeout(() => {
+              chan.publish("", QUEUE, msg.content, {
+                persistent: true,
+                headers: {
+                  "x-retry-count": retryCount,
+                },
+              });
+              chan.ack(msg);
+            }, 1000 * retryCount);
+          } else {
+            console.error(
+              `Max retries reached for job: ${msg.content.toString()}`
+            );
+            chan.reject(msg, false);
+          }
+        }
       } else {
         console.log("Consumer cancelled!");
       }
